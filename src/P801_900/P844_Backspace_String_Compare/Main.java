0dc9d3bd43e9925fd9661c379f63c5819f1337cc
RoMANzhula <roman1985al@ukr.net>
2025-03-10 17:18:55 +0200

    P844_Backspace_String_Compare

---
+++ src/P801_900/P844_Backspace_String_Compare/Main.java
@@ -0,0 +1,53 @@
+package P801_900.P844_Backspace_String_Compare;
+
+public class Main {
+    public static void main(String[] args) {
+        Main solution = new Main();
+
+        System.out.println(solution.backspaceCompare("ab#c", "ad#c")); //true
+        System.out.println(solution.backspaceCompare("ab#cs", "ad#c")); //false
+    }
+
+    public boolean backspaceCompare(String s, String t) {
+        return buildString(s).equals(buildString(t));
+    }
+
+    private String buildString(String input) {
+        StringBuilder result = new StringBuilder();
+
+        for (char c : input.toCharArray()) {
+            if (c == '#') {
+                if (result.length() > 0) {
+                    result.deleteCharAt(result.length() - 1);
+                }
+            } else {
+                result.append(c);
+            }
+        }
+
+        return result.toString();
+    }
+}
+
+//Given two strings s and t, return true if they are equal when both are typed into empty text editors.
+// '#' means a backspace character.
+//Note that after backspacing an empty text, the text will continue empty.
+
+//Example 1:
+//Input: s = "ab#c", t = "ad#c"
+//Output: true
+//Explanation: Both s and t become "ac".
+
+//Example 2:
+//Input: s = "ab##", t = "c#d#"
+//Output: true
+//Explanation: Both s and t become "".
+
+//Example 3:
+//Input: s = "a#c", t = "b"
+//Output: false
+//Explanation: s becomes "c" while t becomes "b".
+
+//Constraints:
+//1 <= s.length, t.length <= 200
+//s and t only contain lowercase letters and '#' characters.
