073b72d45b75acf3eae2182905d206a4196483ee
RoMANzhula <roman1985al@ukr.net>
2026-01-03 18:15:21 +0200

    P1411_Number_of_Ways_to_Paint_N_x_3_Grid

---
+++ src/P1401_1500/P1411_Number_of_Ways_to_Paint_N_x_3_Grid/Main.java
@@ -0,0 +1,54 @@
+package P1401_1500.P1411_Number_of_Ways_to_Paint_N_x_3_Grid;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Main solution = new Main();
+
+        System.out.println(solution.numOfWays(1));     // 12
+        System.out.println(solution.numOfWays(5000));  // 30228214
+    }
+
+    private final int MOD = 1_000_000_007;
+
+    public int numOfWays(int n) {
+        // base case - n = 1
+        long same = 6; // ABA patterns
+        long diff = 6; // ABC patterns
+
+        for (int i = 2; i <= n; i++) {
+            long newSame = (same * 3 + diff * 2) % MOD;
+            long newDiff = (same * 2 + diff * 2) % MOD;
+
+            same = newSame;
+            diff = newDiff;
+        }
+
+        return (int) ((same + diff) % MOD);
+    }
+
+}
+
+//Complexity:
+// time - o(n)
+// space - O(1)
+
+
+//You have a grid of size n x 3 and you want to paint each cell of the grid with exactly one of the three colors: Red,
+// Yellow, or Green while making sure that no two adjacent cells have the same color (i.e., no two cells that share
+// vertical or horizontal sides have the same color).
+//Given n the number of rows of the grid, return the number of ways you can paint this grid. As the answer may grow
+// large, the answer must be computed modulo 109 + 7.
+
+//Example 1:
+//Input: n = 1
+//Output: 12
+//Explanation: There are 12 possible way to paint the grid as shown.
+
+//Example 2:
+//Input: n = 5000
+//Output: 30228214
+
+//Constraints:
+//n == grid.length
+//1 <= n <= 5000
