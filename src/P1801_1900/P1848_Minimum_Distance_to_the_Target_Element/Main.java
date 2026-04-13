b51280465b98e325219e5095ec048dad9dbde086
RoMANzhula <roman1985al@ukr.net>
2026-04-13 19:17:00 +0300

    P1848_Minimum_Distance_to_the_Target_Element

---
+++ src/P1801_1900/P1848_Minimum_Distance_to_the_Target_Element/Main.java
@@ -0,0 +1,63 @@
+package P1801_1900.P1848_Minimum_Distance_to_the_Target_Element;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Main solution = new Main();
+
+        int[] nums1 = {1, 2, 3, 4, 5};
+        System.out.println(solution.getMinDistance(nums1, 5, 3)); // Output: 1
+
+        int[] nums2 = {1};
+        System.out.println(solution.getMinDistance(nums2, 1, 0)); // Output: 0
+
+        int[] nums3 = {1,1,1,1,1,1,1,1,1,1};
+        System.out.println(solution.getMinDistance(nums3, 1, 0)); // Output: 0
+    }
+
+    public int getMinDistance(int[] nums, int target, int start) {
+        int minDistance = Integer.MAX_VALUE;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target) {
+                int distance = Math.abs(i - start);
+                minDistance = Math.min(minDistance, distance);
+            }
+        }
+
+        return minDistance;
+    }
+
+}
+
+//Complexity:
+// time - O(n)
+// space - O(1)
+
+
+//Given an integer array nums (0-indexed) and two integers target and start, find an index i such that
+// nums[i] == target and abs(i - start) is minimized. Note that abs(x) is the absolute value of x.
+//Return abs(i - start).
+//It is guaranteed that target exists in nums.
+
+//Example 1:
+//Input: nums = [1,2,3,4,5], target = 5, start = 3
+//Output: 1
+//Explanation: nums[4] = 5 is the only value equal to target, so the answer is abs(4 - 3) = 1.
+
+//Example 2:
+//Input: nums = [1], target = 1, start = 0
+//Output: 0
+//Explanation: nums[0] = 1 is the only value equal to target, so the answer is abs(0 - 0) = 0.
+
+//Example 3:
+//Input: nums = [1,1,1,1,1,1,1,1,1,1], target = 1, start = 0
+//Output: 0
+//Explanation: Every value of nums is 1, but nums[0] minimizes abs(i - start), which is abs(0 - 0) = 0.
+
+//Constraints:
+//1 <= nums.length <= 1000
+//1 <= nums[i] <= 104
+//0 <= start < nums.length
+//target is in nums.
+//
