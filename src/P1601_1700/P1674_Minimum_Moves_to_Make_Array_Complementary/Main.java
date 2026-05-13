417c4714445de2ec9f9fc38fc68219f4c44e4690
RoMANzhula <roman1985al@ukr.net>
2026-05-13 19:36:43 +0300

    P1674_Minimum_Moves_to_Make_Array_Complementary

---
+++ src/P1601_1700/P1674_Minimum_Moves_to_Make_Array_Complementary/Main.java
@@ -0,0 +1,87 @@
+package P1601_1700.P1674_Minimum_Moves_to_Make_Array_Complementary;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Main solution = new Main();
+
+        System.out.println(solution.minMoves(new int[]{1,2,4,3}, 4)); // 1
+        System.out.println(solution.minMoves(new int[]{1,2,2,1}, 2)); // 2
+        System.out.println(solution.minMoves(new int[]{1,2,1,2}, 2)); // 0
+    }
+
+    public int minMoves(int[] nums, int limit) {
+        int n = nums.length;
+
+        // difference array for sweep line
+        int[] diff = new int[2 * limit + 2];
+
+        for (int i = 0; i < n / 2; i++) {
+            int a = nums[i];
+            int b = nums[n - 1 - i];
+
+            int low = Math.min(a, b) + 1;
+            int high = Math.max(a, b) + limit;
+            int sum = a + b;
+
+            // init assume 2 moves
+            diff[2] += 2;
+
+            // 1 move zone
+            diff[low] -= 1;
+            diff[high + 1] += 1;
+
+            // 0 move at exact sum
+            diff[sum] -= 1;
+            diff[sum + 1] += 1;
+        }
+
+        int answer = Integer.MAX_VALUE;
+        int curr = 0;
+
+        for (int s = 2; s <= 2 * limit; s++) {
+            curr += diff[s];
+            answer = Math.min(answer, curr);
+        }
+
+        return answer;
+    }
+
+}
+
+//Complexity:
+// time - O(n + limit)
+// space - O(limit)
+
+
+//You are given an integer array nums of even length n and an integer limit. In one move, you can replace any
+// integer from nums with another integer between 1 and limit, inclusive.
+//The array nums is complementary if for all indices i (0-indexed), nums[i] + nums[n - 1 - i] equals the same number.
+// For example, the array [1,2,3,4] is complementary because for all indices i, nums[i] + nums[n - 1 - i] = 5.
+//Return the minimum number of moves required to make nums complementary.
+
+//Example 1:
+//Input: nums = [1,2,4,3], limit = 4
+//Output: 1
+//Explanation: In 1 move, you can change nums to [1,2,2,3] (underlined elements are changed).
+//nums[0] + nums[3] = 1 + 3 = 4.
+//nums[1] + nums[2] = 2 + 2 = 4.
+//nums[2] + nums[1] = 2 + 2 = 4.
+//nums[3] + nums[0] = 3 + 1 = 4.
+//Therefore, nums[i] + nums[n-1-i] = 4 for every i, so nums is complementary.
+
+//Example 2:
+//Input: nums = [1,2,2,1], limit = 2
+//Output: 2
+//Explanation: In 2 moves, you can change nums to [2,2,2,2]. You cannot change any number to 3 since 3 > limit.
+
+//Example 3:
+//Input: nums = [1,2,1,2], limit = 2
+//Output: 0
+//Explanation: nums is already complementary.
+
+//Constraints:
+//n == nums.length
+//2 <= n <= 105
+//1 <= nums[i] <= limit <= 105
+//n is even.
